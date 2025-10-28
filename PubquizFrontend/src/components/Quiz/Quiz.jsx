537f603f755644d22da1d629a87cecf926cad857
Sjors-Hoogenboom <SjorsH.2004@gmail.com>
2025-10-28 14:44:36 +0100

    Frontend made ready to receive quiz from backend

--- PubquizFrontend/src/components/Quiz/Quiz.jsx
+++ PubquizFrontend/src/components/Quiz/Quiz.jsx
@@ -1,11 +1,14 @@
-import {useCallback, useState} from "react";
+import {useCallback, useEffect, useState} from "react";
 import QUESTIONS from '../../data/questions.js'
 import Question from './Question.jsx'
 import './Quiz.css'
 import Summary from './Summary.jsx'
+import ErrorPage from '../Error/Error.jsx'
 
 export default function Quiz() {
     const [userAnswers, setUserAnswers] = useState([])
+    const [isFetching, setIsFetching] = useState(false);
+    const [error, setError] = useState();
 
     const activeQuestionIndex = userAnswers.length;
     const quizIsComplete = activeQuestionIndex === QUESTIONS.length
@@ -15,8 +18,37 @@ export default function Quiz() {
             return [...prevUserAnswers, selectedAnswer];
         });
     }, [])
-
     const handleSkipAnswer = useCallback(() => handleSelectAnswer(null), [handleSelectAnswer])
+
+    useEffect(() => {
+        async function fetchQuiz() {
+            setIsFetching(true);
+
+            try {
+                const response = await fetch("ff");
+
+                if (!response.ok) {
+                    throw new Error("Failed to fetch quiz")
+                }
+                const responseData = await response.json();
+            }
+            catch (error) {
+                setError({message: error.message || 'Could not fetch quiz'});
+            }
+            finally {
+                setIsFetching(false);
+            }
+        }
+        fetchQuiz();
+    }, []);
+
+    if (error) {
+        return <ErrorPage title="An error occurred" message={error.message ?? String(error)} />;
+    }
+
+    if (isFetching) {
+        return <p>Loading...</p>;
+    }
 
     if (quizIsComplete) {
         return (
